8c69738399dfa6ecee11c80f8bf02ec8396a7413
Brian Ketelsen <bketelsen@gmail.com>
2021-07-04 08:40:23 -0400

    more cleanup

--- .zsh/includes/cleanup.sh
+++ .zsh/includes/cleanup.sh
@@ -2,6 +2,16 @@ cleanup () {
     find ~/src -name 'node_modules' -type d -prune -exec rm -rf '{}' +
     find ~/src -name '.next' -type d -prune -exec rm -rf '{}' +
     cargo sweep -r -t 30 ~/src
+    brew cleanup
+    is_bin_in_path docker && docker system prune || echo "skipping docker"
+}
+# https://stackoverflow.com/questions/6569478/detect-if-executable-file-is-on-users-path
+function is_bin_in_path {
+  if [[ -n $ZSH_VERSION ]]; then
+    builtin whence -p "$1" &> /dev/null
+  else  # bash:
+    builtin type -P "$1" &> /dev/null
+  fi
 }
 
 
