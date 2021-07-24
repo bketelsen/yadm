ea2315b465a9c91f82588bf132467fe82e216e4a
Brian Ketelsen <bketelsen@gmail.com>
2021-07-24 15:07:15 -0400

    stupid boy

--- .zsh/includes/cleanup.sh
+++ .zsh/includes/cleanup.sh
@@ -1,7 +1,6 @@
 cleanup () {
     find ~/src -name 'node_modules' -type d -prune -exec rm -rf '{}' +
     find ~/src -name '.next' -type d -prune -exec rm -rf '{}' +
-    find ~/src -name '.stfolder' -type d -prune -exec rm -rf '{}' +
     cargo sweep -r -t 30 ~/src
     brew cleanup
     is_bin_in_path docker && docker system prune -a -f --volumes || echo "skipping docker"
