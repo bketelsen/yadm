52dd2d789b61274ea32e45f470f035c27bf4788b
Brian Ketelsen <bketelsen@gmail.com>
2021-10-26 06:59:49 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo curl -fLo /usr/local/bin/yadm https://github.com/TheLocehiliosan/yadm/raw/master/yadm && sudo chmod a+x /usr/local/bin/yadm
-yadm clone git@github.com:bketelsen/yadm
+yadm clone https://github.com/bketelsen/yadm
